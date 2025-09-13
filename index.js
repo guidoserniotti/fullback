644bcbe3514708b6cec47c4fc1c8683b57f3a2c3
guidoserniotti <guido.serniotti.13@gmail.com>
2025-09-13 19:56:05 -0300

    installed cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const date = new Date();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 var morgan = require("morgan");
 
