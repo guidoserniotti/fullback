e6bee0ddefe739322e2d29b7cc7e5f1261d55fd2
guidoserniotti <guido.serniotti.13@gmail.com>
2025-09-13 19:17:54 -0300

    part3 ex3.8

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 var morgan = require("morgan");
-app.use(morgan("tiny"));
+
+morgan.token("content", function getContent(req) {
+    return JSON.stringify(req.body);
+});
+
+app.use(
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :content"
+    )
+);
 
 let persons = [
     {
