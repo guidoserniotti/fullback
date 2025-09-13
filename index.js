0a0c92cf48d7b5f79d6ec13a988551cf84f18dc5
guidoserniotti <guido.serniotti.13@gmail.com>
2025-09-13 18:00:30 -0300

    part3 ex3.5

--- index.js
+++ index.js
@@ -54,6 +54,29 @@ app.get("/api/persons/:id", (req, res) => {
     }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    console.log(persons.find((person) => person.id === id));
+    persons = persons.filter((person) => person.id !== id);
+    res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body;
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: "person data missing",
+        });
+    }
+    const person = {
+        id: Math.floor(Math.random() * 1000),
+        name: body.name,
+        number: body.number,
+    };
+    persons = persons.concat(person);
+    res.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
