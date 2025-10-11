84d7a7db9d7f3baee08cefd7efd1b1e08bc00ccf
guidoserniotti <guido.serniotti.13@gmail.com>
2025-10-11 11:05:08 -0300

    refactor project files root

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,23 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({});
-    response.json(blogs);
+blogRouter.get("/", async (request, response, next) => {
+    try {
+        const blogs = await Blog.find({});
+        response.json(blogs);
+    } catch (error) {
+        next(error);
+    }
 });
 
-blogRouter.post("/", async (request, response) => {
+blogRouter.post("/", async (request, response, next) => {
     const blog = new Blog(request.body);
     try {
         const savedBlog = await blog.save();
         response.status(201).json(savedBlog);
     } catch (error) {
         response.status(400).json({ error: error.message });
+        next(error);
     }
 });
 
